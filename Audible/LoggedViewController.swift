78579a492f0e5a0f72070a4ba2e62fe53312b70c
AndyStef <Andy.stef.2512@gmail.com>
2017-04-06 18:58:07 +0300

    hijacking start controller

---
+++ Audible/LoggedViewController.swift
@@ -0,0 +1,40 @@
+//
+//  LoggedViewController.swift
+//  Audible
+//
+//  Created by Stef on 4/6/17.
+//  Copyright © 2017 Stef. All rights reserved.
+//
+
+import UIKit
+
+class LoggedViewController: UINavigationController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+
+        if isLoggedIn() {
+            let homeController = HomeController()
+            viewControllers = [homeController]
+        } else {
+            perform(#selector(showLoginController), with: nil, afterDelay: 0.01)
+        }
+    }
+    
+    fileprivate func isLoggedIn() -> Bool {
+        return true
+    }
+    
+    func showLoginController() {
+        let loginController = MainViewController()
+        present(loginController, animated: true, completion: nil)
+    }
+}
+
+class HomeController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .yellow
+    }
+}
