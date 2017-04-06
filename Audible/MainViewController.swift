9ab9f4dd78edfe989d389676f5621f5d06e2d85b
AndyStef <Andy.stef.2512@gmail.com>
2017-04-06 16:16:37 +0300

    Added pageControl and side buttons

--- Audible/MainViewController.swift
+++ Audible/MainViewController.swift
@@ -27,6 +27,34 @@ class MainViewController: UIViewController {
         return collectionView
     }()
     
+    private let pageControl: UIPageControl = {
+        let pageControl = UIPageControl()
+        pageControl.translatesAutoresizingMaskIntoConstraints = false
+        pageControl.pageIndicatorTintColor = .lightGray
+        pageControl.numberOfPages = 3
+        pageControl.currentPageIndicatorTintColor = UIColor(colorLiteralRed: 247/255, green: 154/255, blue: 27/255, alpha: 1.0)
+        
+        return pageControl
+    }()
+    
+    private let skipButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Skip", for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setTitleColor(UIColor(colorLiteralRed: 247/255, green: 154/255, blue: 27/255, alpha: 1.0), for: .normal)
+        
+        return button
+    }()
+    
+    private let nextButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Next", for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setTitleColor(UIColor(colorLiteralRed: 247/255, green: 154/255, blue: 27/255, alpha: 1.0), for: .normal)
+        
+        return button
+    }()
+    
     //MARK: - there is always better place and name for this
     fileprivate let cellId = "CellId"
     fileprivate let pages: [Page] = {
@@ -53,8 +81,30 @@ class MainViewController: UIViewController {
     private func configureCollectionView() {
         collectionView.register(PageCollectionViewCell.self, forCellWithReuseIdentifier: cellId)
         view.addSubview(collectionView)
+        view.addSubview(pageControl)
+        view.addSubview(skipButton)
+        view.addSubview(nextButton)
+        
+        //page control anchors
+        NSLayoutConstraint.activate(
+            [pageControl.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+             pageControl.leftAnchor.constraint(equalTo: view.leftAnchor),
+             pageControl.rightAnchor.constraint(equalTo: view.rightAnchor),
+             pageControl.heightAnchor.constraint(equalToConstant: 30.0)])
+        //CV anchors
         NSLayoutConstraint.activate(
             [collectionView.topAnchor.constraint(equalTo: view.topAnchor), collectionView.bottomAnchor.constraint(equalTo: view.bottomAnchor), collectionView.leftAnchor.constraint(equalTo: view.leftAnchor),collectionView.rightAnchor.constraint(equalTo: view.rightAnchor)])
+        //buttons anchors
+        NSLayoutConstraint.activate(
+            [skipButton.topAnchor.constraint(equalTo: view.topAnchor, constant: 16.0),
+             skipButton.leftAnchor.constraint(equalTo: view.leftAnchor),
+             skipButton.widthAnchor.constraint(equalToConstant: 60.0),
+             skipButton.heightAnchor.constraint(equalToConstant: 50.0)])
+        NSLayoutConstraint.activate(
+            [nextButton.topAnchor.constraint(equalTo: view.topAnchor, constant: 16.0),
+             nextButton.rightAnchor.constraint(equalTo: view.rightAnchor),
+             nextButton.widthAnchor.constraint(equalToConstant: 60.0),
+             nextButton.heightAnchor.constraint(equalToConstant: 50.0)])
     }
 }
 
